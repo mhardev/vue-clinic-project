5334f955794ab6ebb40532a751c8c10263021097
Joshua Mhar <joshuamhar05@gmail.com>
2025-02-17 00:49:34 +0800

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import MenuButton from './MenuButton.vue'
+import GetStartButton from './GetStartButton.vue'
+import { ref, onMounted } from 'vue'
+import { Collapse } from 'flowbite'
+
+const Links = ref([
+  { name: 'Home', link: '#' },
+  { name: 'About', link: '#' },
+  { name: 'Service', link: '#' },
+  { name: 'Contact', link: '#' },
+])
+
+let collapse
+
+onMounted(() => {
+  const $targetMenu = document.getElementById('targetMenu')
+  const $openMenu = document.getElementById('openMenu')
+
+  const options = {
+    onCollapse: () => {
+      console.log('element has been collapsed')
+    },
+    onExpand: () => {
+      console.log('element has been expanded')
+    },
+    onToggle: () => {
+      console.log('element has been toggled')
+    },
+  }
+
+  collapse = new Collapse($targetMenu, $openMenu, options)
+})
+</script>
+
+<template>
+  <nav
+    class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
+  >
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+      </a>
+      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <GetStartButton />
+        <MenuButton :collapse="collapse" />
+      </div>
+      <div
+        @click="collapse.toggle()"
+        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        id="targetMenu"
+      >
+        <ul
+          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+        >
+          <li v-for="link in Links" :key="link.name">
+            <a
+              :href="link.link"
+              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >{{ link.name }}</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/pages/ContactPage.vue
@@ -0,0 +1,109 @@
+<script setup></script>
+
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2
+        class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+      >
+        Contact Us
+      </h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        Got a technical issue? Want to send feedback about a beta feature? Need details about our
+        Business plan? Let us know.
+      </p>
+      <form action="#" class="space-y-8">
+        <div>
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Your email</label
+          >
+          <input
+            type="email"
+            id="email"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="john.doe@example.com"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="subject"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Subject</label
+          >
+          <input
+            type="text"
+            id="subject"
+            class="block p-3 w-full text-sm text-gray-600 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="Let us know how we can help you"
+            required
+          />
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="message"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >Your message</label
+          >
+          <textarea
+            id="message"
+            rows="6"
+            class="block p-2.5 w-full text-sm text-gray-600 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Leave a comment..."
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          class="py-3 px-5 text-white text-sm font-medium text-center rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Send message
+        </button>
+      </form>
+    </div>
+  </section>
+
+  <section class="bg-white dark:bg-gray-900 mb-8">
+    <div class="px-4 mx-auto max-w-screen-md">
+      <h2
+        class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+      >
+        Medical Clinic
+      </h2>
+      <p class="mb-8 lg:mb-4 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </p>
+      <div class="mapouter py-0 px-4 mx-auto max-w-screen-md">
+        <div class="gmap_canvas">
+          <iframe
+            class="gmap_iframe"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          ></iframe>
+          <a href="https://sprunkin.com/">Sprunki Game</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.mapouter {
+  position: relative;
+  text-align: right;
+  width: 600px;
+  height: 400px;
+}
+.gmap_canvas {
+  overflow: hidden;
+  background: none !important;
+  width: 600px;
+  height: 400px;
+}
+.gmap_iframe {
+  width: 600px !important;
+  height: 400px !important;
+}
+</style>
