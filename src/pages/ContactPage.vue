d96ca64cb67cb2d68345199600d0d41e4abb8a5e
Joshua Mhar <joshuamhar05@gmail.com>
2025-02-19 12:16:16 +0800

    refactor: update ContactPage title and embed Google Maps for improved location accuracy

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -69,7 +69,7 @@
       <h2
         class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
       >
-        Medical Clinic
+        CV Medical Clinic
       </h2>
       <p class="mb-8 lg:mb-4 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -82,7 +82,7 @@
             scrolling="no"
             marginheight="0"
             marginwidth="0"
-            src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.074006609081!2d121.03708787575009!3d14.708406785790816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b0e6dec442af%3A0xe6850fa9dfba5de6!2sPadron%20Medical%20Clinic!5e0!3m2!1sen!2sph!4v1739930536234!5m2!1sen!2sph"
           ></iframe>
         </div>
       </div>
@@ -106,5 +106,11 @@
 .gmap_iframe {
   width: 100%;
   height: 100%;
+}
+
+element.style {
+  /* position: absolute; */
+  left: 0px;
+  top: 0px;
 }
 </style>
