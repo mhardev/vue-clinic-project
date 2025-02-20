5318c5832d01762f1df6dfdb868fd026172f2a1d
Joshua Mhar <joshuamhar05@gmail.com>
2025-02-20 23:36:42 +0800

    refactor: reconfigured all dependencies

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  define: {
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
   },
 })
